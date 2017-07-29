6d045905dcc265404d81565e0a71ca63d113d897
Sam Butler-Thompson <sam@q2q-it.com>
2017-07-29 11:54:37 +0100

    Created first view wireframe, Panel component
    Created basic Panel parent class, RoutinePanel subclass, implemented switch router
    
    Closes #1
    Closes #2

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
+
+import RoutinePanel from './components/panel/subs/Routine';
 
 class App extends Component {
   render() {
@@ -11,9 +13,12 @@ class App extends Component {
         <div className="titleBar">
           <h1>Hello world!</h1>
         </div>
+        <Switch>
+          <Route exact path="/" component={RoutinePanel}/>
+        </Switch>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
