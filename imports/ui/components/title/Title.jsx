d02a1a1278eb854b21de846f2c4b5cffdaebc95a
Sam Butler-Thompson <sam@q2q-it.com>
2017-07-29 12:01:57 +0100

    Added base navigation components
    Added Navigation and NavLinkItem component, rendered Navigation within Title component.
    
    Closes #5

--- imports/ui/components/title/Title.jsx
+++ imports/ui/components/title/Title.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 
+import Navigation from '../navigation/Navigation';
+
 class Title extends Component {
   render() {
     return (
       <div className="titleBar">
         <h1>Todo App</h1>
+        <Navigation/>
       </div>
     )
   }
